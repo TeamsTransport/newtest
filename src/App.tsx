618526d25af24341e46dfcc648d748cc492932de
TEAMS Transport <carey@teamstransport.com>
2025-09-19 16:03:07 -0500

    updated nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,41 @@
-import { Routes, Route, Link } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
-
-
-
-    <div className="navbar bg-primary text-primary-content shadow-sm">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-            <li><Link to="/" >Home</link></li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
+    <div className="p-4 bg-base-100">
+      <div className="navbar bg-base-200 rounded-box">
+        <div className="flex-1">
+          <Link to="/" className="btn btn-ghost text-xl">My App</Link>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li><Link to="/">Home</Link></li>
+            <li tabIndex={0}>
+              <a className="justify-between">
+                Parent
+                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7 10l5 5 5-5z"/></svg>
+              </a>
+              <ul className="p-2 bg-base-100 rounded-box z-10">
+                <li><Link to="/submenu1">Submenu 1</Link></li>
+                <li><Link to="/submenu2">Submenu 2</Link></li>
               </ul>
             </li>
-            <li><Link to="/about" >About</Link></li>
+            <li><Link to="/about">About</Link></li>
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">Case Inventory</a>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li><Link to="/" >Home</link></li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </details>
-          </li>
-          <li><Link to="/about" >About</Link></li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <a className="btn">Button</a>
-      </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* Add these if you have components for submenu pages */}
+        {/* <Route path="/submenu1" element={<Submenu1 />} />
+        <Route path="/submenu2" element={<Submenu2 />} /> */}
+      </Routes>
     </div>
-
-
-
-  )
+  );
 }
 
-export default App
+export default App;
