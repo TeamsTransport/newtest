9cac4fe3da012e019405b550ca41bc6f1a178f09
TEAMS Transport <carey@teamstransport.com>
2025-09-27 21:53:20 -0500

    Added state to hide nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,67 @@
-import { useState } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 
 function App() {
-  const [showSubmenu, setShowSubmenu] = useState(false);
-
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">My App</h1>
-
-      <nav className="space-x-4">
-        <Link to="/" className="btn btn-link">Home</Link>
-
-        <div className="inline-block relative">
-          <button
-            onClick={() => setShowSubmenu(!showSubmenu)}
-            className="btn btn-link"
-          >
-            Parent
-          </button>
-
-          {showSubmenu && (
-            <div className="absolute bg-white shadow rounded mt-2 p-2 space-y-2">
-              <Link to="/submenu1" className="block">Submenu 1</Link>
-              <Link to="/submenu2" className="block">Submenu 2</Link>
+    <div>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
             </div>
-          )}
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow"
+            >
+              <li><Link to="/">Item 1</Link></li>
+              <li>
+                <a>Parent</a>
+                <ul className="p-2">
+                  <li><Link to="/submenu1">Submenu 1</Link></li>
+                  <li><Link to="/submenu2">Submenu 2</Link></li>
+                </ul>
+              </li>
+              <li><Link to="/about">Item 3</Link></li>
+            </ul>
+          </div>
+          <Link to="/" className="btn btn-ghost text-xl">daisyUI</Link>
         </div>
 
-        <Link to="/about" className="btn btn-link">About</Link>
-      </nav>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li><Link to="/">Item 1</Link></li>
+            <li>
+              <details>
+                <summary>Parent</summary>
+                <ul className="p-2">
+                  <li><Link to="/submenu1">Submenu 1</Link></li>
+                  <li><Link to="/submenu2">Submenu 2</Link></li>
+                </ul>
+              </details>
+            </li>
+            <li><Link to="/about">Item 3</Link></li>
+          </ul>
+        </div>
+
+        <div className="navbar-end">
+          <a className="btn">Button</a>
+        </div>
+      </div>
 
       <Routes>
         <Route path="/" element={<Home />} />
