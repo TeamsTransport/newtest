e9eaa57c4361e20bac67211341523bd6808141ea
TEAMS Transport <carey@teamstransport.com>
2025-09-27 21:56:01 -0500

    Added state to hide nav

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ function App() {
     <div>
       <div className="navbar bg-base-100 shadow-sm">
         <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+          <div className="dropdown dropdown-hover">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
@@ -23,16 +23,16 @@ function App() {
                   d="M4 6h16M4 12h8m-8 6h16"
                 />
               </svg>
-            </div>
+            </label>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow"
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
               <li><Link to="/">Item 1</Link></li>
               <li>
                 <a>Parent</a>
                 <ul className="p-2">
-                  <li><Link to="/submenu1">Submenu 1</Link></li>
+ to="/submenu1">Submenu 1</Link></li>
                   <li><Link to="/submenu2">Submenu 2</Link></li>
                 </ul>
               </li>
@@ -45,14 +45,15 @@ function App() {
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
             <li><Link to="/">Item 1</Link></li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li><Link to="/submenu1">Submenu 1</Link></li>
-                  <li><Link to="/submenu2">Submenu 2</Link></li>
-                </ul>
-              </details>
+            <li className="dropdown dropdown-hover">
+              <label tabIndex={0} className="btn btn-ghost">Parent</label>
+              <ul
+                tabIndex={0}
+                className="menu dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li><Link to="/submenu1">Submenu 1</Link></li>
+                <li><Link to="/submenu2">Submenu 2</Link></li>
+              </ul>
             </li>
             <li><Link to="/about">Item 3</Link></li>
           </ul>
@@ -66,7 +67,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        {/* Add submenu routes if needed */}
         {/* <Route path="/submenu1" element={<Submenu1 />} /> */}
         {/* <Route path="/submenu2" element={<Submenu2 />} /> */}
       </Routes>
