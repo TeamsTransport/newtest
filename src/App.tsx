e93338141aa9c0c67612cfdb468297ec72fd1b57
TEAMS Transport <carey@teamstransport.com>
2025-09-27 22:53:39 -0500

    Added state to hide nav

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,17 @@ function App() {
     <div>
       <div className="navbar bg-base-100 shadow-sm">
         <div className="navbar-start">
-          <div className="dropdown dropdown-hover">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16"/>
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
               </svg>
-            </label>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            </div>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
               <li><Link to="/">Item 1</Link></li>
-              <li className="dropdown dropdown-hover dropdown-right">
-                <a className="font-semibold">Parent</a>
-                <ul className="dropdown-content menu menu-sm shadow bg-base-100 rounded-box w-52 z-20">
+              <li>
+                <a>Parent</a>
+                <ul className="p-2">
                   <li><Link to="/submenu1">Submenu 1</Link></li>
                   <li><Link to="/submenu2">Submenu 2</Link></li>
                 </ul>
@@ -30,9 +30,9 @@ function App() {
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
             <li><Link to="/">Item 1</Link></li>
-            <li className="relative group">
-              <Link to="#" className="btn btn-ghost">Parent</Link>
-              <ul tabIndex={0} className="absolute hidden group-hover:flex flex-col bg-base-100 shadow rounded-box mt-2 p-2 z-10">
+            <li className="dropdown dropdown-hover">
+              <label tabIndex={0} className="btn btn-ghost">Parent</label>
+              <ul tabIndex={0} className="dropdown-content menu menu-sm z-[1] mt-3 w-52 p-2 shadow bg-base-100 rounded-box">
                 <li><Link to="/submenu1">Submenu 1</Link></li>
                 <li><Link to="/submenu2">Submenu 2</Link></li>
               </ul>
