13e8bec7e213f4ad8b3d0b3318abcaa8ab47de8b
TEAMS Transport <carey@teamstransport.com>
2025-09-27 22:22:11 -0500

    Added state to hide nav

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,11 @@ function App() {
         <div className="navbar-start">
           <div className="dropdown dropdown-hover">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16"/>
               </svg>
             </label>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-            >
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
               <li><Link to="/">Item 1</Link></li>
               <li>
                 <a>Parent</a>
@@ -41,28 +27,23 @@ function App() {
           </div>
           <Link to="/" className="btn btn-ghost text-xl">daisyUI</Link>
         </div>
-
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
             <li><Link to="/">Item 1</Link></li>
             <li className="relative group">
               <Link to="#" className="btn btn-ghost">Parent</Link>
-              <ul className="absolute hidden group-hover:flex flex-col bg-base-100 shadow rounded-box mt-2 p-2 z-10">
+              <ul tabIndex={0} className="absolute hidden group-hover:flex flex-col bg-base-100 shadow rounded-box mt-2 p-2 z-10">
                 <li><Link to="/submenu1">Submenu 1</Link></li>
                 <li><Link to="/submenu2">Submenu 2</Link></li>
               </ul>
             </li>
-                tabIndex={0}
-                className="menu dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-              >
             <li><Link to="/about">Item 3</Link></li>
+          </ul>
         </div>
-
         <div className="navbar-end">
           <a className="btn">Button</a>
         </div>
       </div>
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
